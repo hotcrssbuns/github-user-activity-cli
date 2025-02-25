deef12d32d4955be26ee180911ec3b3a4d996f24
“Eren” <135561434+hotcrssbuns@users.noreply.github.com>
2025-02-24 20:51:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from urllib import request
+from urllib import request, error
 import json
 
 
 def fetch_json(link):
-    resp = request.urlopen(link)
-    data = resp.read()
-    string_data = data.decode("UTF-8")
-    json_data = json.loads(string_data)
-    return json.dumps(json_data, indent=2)
+    try:
+        resp = request.urlopen(link)
+        data = resp.read()
+        string_data = data.decode("UTF-8")
+        json_data = json.loads(string_data)
+        return json.dumps(json_data, indent=2)
+    except error.HTTPError as e:
+        print(f"HTTP Error occured")
 
 
-username = input("github-activity ")
+def main():
+    username = input("github-activity ")
+    link = f"https://api.github.com/users/{username}/events"
+    formatted_json = fetch_json(link)
 
-link = f"https://api.github.com/users/{username}/events"
+    print(formatted_json)
 
-formatted_json = fetch_json(link)
 
-print(formatted_json)
+main()
