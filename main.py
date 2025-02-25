a449cffd34b4125ce3a1e3ab6b5f9384e8b38ed5
“Eren” <135561434+hotcrssbuns@users.noreply.github.com>
2025-02-24 18:39:31 -0600

    Moved json fetcher to function

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from urllib import request
 import json
 
+
+def fetch_json(link):
+    resp = request.urlopen(link)
+    data = resp.read()
+    string_data = data.decode("UTF-8")
+    json_data = json.loads(string_data)
+    return json.dumps(json_data, indent=2)
+
+
 username = input("github-activity ")
 
 link = f"https://api.github.com/users/{username}/events"
 
-resp = request.urlopen(link)
-
-data = resp.read()
-
-string_data = data.decode("UTF-8")
-
-json_data = json.loads(string_data)
-
-formatted_json = json.dumps(json_data, indent=2)
+formatted_json = fetch_json(link)
 
 print(formatted_json)
