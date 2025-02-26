2bcaee8b9933c6e11a4b463247082b650f18f046
“Eren” <135561434+hotcrssbuns@users.noreply.github.com>
2025-02-25 18:14:59 -0600

    Added incrementation for dictionary

--- main.py
+++ main.py
@@ -21,20 +21,29 @@ def fetch_json(link):
 def main():
     username = input("github-activity ")
     link = f"https://api.github.com/users/{username}/events"
+    events = {}
     formatted_json = fetch_json(link)
     clear()
     print("Output:")
 
+    # Go through JSON file and update dictionary with number of times an event shows up
     for value in formatted_json:
         event_type = value["type"]
         repo_name = value["repo"]["name"]
+        if f"{event_type}_{repo_name}" in events.keys():
+            events[f"{event_type}_{repo_name}"] += 1
+        else:
+            events.update({f"{event_type}_{repo_name}": 1})
 
-        if event_type == "PublicEvent":
-            print(f"- Changed repository from private to public {repo_name}")
-        if event_type == "WatchEvent":
-            print(f"- Starred repository {repo_name}")
-        if event_type == "PushEvent":
-            print(f"- Pushed commit to {repo_name}")
+        # if event_type == "PublicEvent":
+        # print(f"- Changed repository from private to public {repo_name}")
+        # if event_type == "WatchEvent":
+        # print(f"- Starred repository {repo_name}")
+        # if event_type == "PushEvent":
+        # print(f"- Pushed commit to {repo_name}")
+
+    for key in events.keys():
+        print(key)
 
 
 main()
