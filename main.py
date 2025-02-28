1fa7c38d28fa647bb73a9872d2df49530096cbb6
“Eren” <135561434+hotcrssbuns@users.noreply.github.com>
2025-02-27 18:10:26 -0600

    Added error checking for empty json files

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
 
+def is_json_empty(json):
+    return len(json) == 0
+
+
 def fetch_json(link):
     try:
         resp = request.urlopen(link)
@@ -15,25 +19,33 @@ def fetch_json(link):
         json_data = json.loads(string_data)
         return json_data
     except error.HTTPError as e:
-        print(f"HTTP Error occured")
+        print(f"Error occured: {e}")
 
 
 def main():
-    username = input("github-activity ")
-    link = f"https://api.github.com/users/{username}/events"
-    events = {}
-    formatted_json = fetch_json(link)
+    while True:
+        username = input("github-activity ")
+        link = f"https://api.github.com/users/{username}/events"
+        events = {}
+        formatted_json = fetch_json(link)
+        if is_json_empty(formatted_json):
+            print("Error: Not legitimate account. Try another")
+        else:
+            break
     clear()
     print("Output:")
 
     # Go through JSON file and update dictionary with number of times an event shows up
-    for value in formatted_json:
-        event_type = value["type"]
-        repo_name = value["repo"]["name"]
-        if f"{event_type}_{repo_name}" in events.keys():
-            events[f"{event_type}_{repo_name}"] += 1
-        else:
-            events.update({f"{event_type}_{repo_name}": 1})
+    try:
+        for value in formatted_json:
+            event_type = value["type"]
+            repo_name = value["repo"]["name"]
+            if f"{event_type}_{repo_name}" in events.keys():
+                events[f"{event_type}_{repo_name}"] += 1
+            else:
+                events.update({f"{event_type}_{repo_name}": 1})
+    except TypeError as e:
+        print(f"Error: {e}")
 
     for key, value in events.items():
         event_type, repo_name = key.split("_")
